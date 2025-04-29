68c75535bacb76f8ca61094b544b978edd769ce0
Yuni Wu <blog880203@gmail.com>
2025-04-29 00:59:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -131,4 +131,6 @@ def callback():
     return f"Authorization code received: {code}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
