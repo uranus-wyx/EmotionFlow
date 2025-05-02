bf78b699cbafa3ca6eb03b0d0e38e9f9703b4179
Yuni-wih <55570963+Yuni-wih@users.noreply.github.com>
2025-05-01 20:03:33 -0600

    change

--- secret.py
+++ secret.py
@@ -5,8 +5,8 @@ import os
 from dotenv import load_dotenv
 
 def get_secret(secret_id: str, version: str = "latest") -> str:
-    # project_id = os.getenv("GCP_PROJECT") or os.getenv("GOOGLE_CLOUD_PROJECT")
-    project_id = "the-mesh-458219-a9"
+    project_id = os.getenv("GCP_PROJECT") or os.getenv("GOOGLE_CLOUD_PROJECT")
+    # project_id = "the-mesh-458219-a9"
     if not project_id:
         raise RuntimeError("GCP project ID not found in environment variables.")
 
